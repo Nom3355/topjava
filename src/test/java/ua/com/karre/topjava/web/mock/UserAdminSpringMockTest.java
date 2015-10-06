5059117d7f0c7f77ea6cbba308f3673bce7d60e7
nomHome <vladv3355@gmail.com>
2015-10-06 12:41:14 +0300

    Add Spring-Test

---
+++ src/test/java/ua/com/karre/topjava/web/mock/UserAdminSpringMockTest.java
@@ -0,0 +1,39 @@
+package ua.com.karre.topjava.web.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.com.karre.topjava.model.Role;
+import ua.com.karre.topjava.model.User;
+import ua.com.karre.topjava.util.exception.NotFoundException;
+import ua.com.karre.topjava.web.user.AdminUserRestController;
+
+
+/**
+ * GKislin
+ * 13.03.2015.
+ */
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserAdminSpringMockTest {
+
+    @Autowired
+    private AdminUserRestController controller;
+
+    @Test
+    public void testCreate() throws Exception {
+        controller.create(new User(null, "Name", "email@ya.ru", "password", true, Role.ROLE_USER));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        controller.delete(7);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        controller.delete(0);
+    }
+}
