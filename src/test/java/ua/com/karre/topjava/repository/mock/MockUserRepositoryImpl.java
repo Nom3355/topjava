d8267355cc9ebcef3776959e5678afa289ea1270
nomHome <vladv3355@gmail.com>
2015-10-06 12:35:17 +0300

    Add JUnit and test MockRepository implementation

--- src/test/java/ua/com/karre/topjava/repository/mock/MockUserRepositoryImpl.java
+++ src/test/java/ua/com/karre/topjava/repository/mock/MockUserRepositoryImpl.java
@@ -5,6 +5,8 @@ import ua.com.karre.topjava.LoggerWrapper;
 import ua.com.karre.topjava.model.User;
 import ua.com.karre.topjava.repository.UserRepository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,10 +18,20 @@ import java.util.List;
 public class MockUserRepositoryImpl implements UserRepository {
     private static final LoggerWrapper LOG = LoggerWrapper.get(MockUserRepositoryImpl.class);
 
+    @PostConstruct
+    public void postConstruct() {
+        LOG.info("PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        LOG.info("PreDestroy");
+    }
+
     @Override
     public boolean delete(int id) {
         LOG.info("delete " + id);
-        return true;
+        return id != 0;
     }
 
     @Override
