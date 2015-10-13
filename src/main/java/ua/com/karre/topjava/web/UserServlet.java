fd49fcd46a4e015ea74b6ecb3a89f502080d5fb6
nomHome <vladv3355@gmail.com>
2015-10-13 11:52:35 +0300

    Add spring-web Initialize web spring context

--- src/main/java/ua/com/karre/topjava/web/UserServlet.java
+++ src/main/java/ua/com/karre/topjava/web/UserServlet.java
@@ -1,7 +1,11 @@
 package ua.com.karre.topjava.web;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import ua.com.karre.topjava.LoggerWrapper;
+import ua.com.karre.topjava.service.UserService;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +14,19 @@ import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
     private static final LoggerWrapper LOG = LoggerWrapper.get(UserServlet.class);
+    private WebApplicationContext wac;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        wac = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to userList");
-
-//        request.getRequestDispatcher("/userList.jsp").forward(request, response);
-        response.sendRedirect("userList.jsp");
+        UserService userService = wac.getBean(UserService.class);
+        request.setAttribute("userList", userService.getAll());
+        request.getRequestDispatcher("/userList.jsp").forward(request, response);
+//        response.sendRedirect("userList.jsp");
     }
 }
